b6473cc48cd434c15f1a6bd9f06172c73b34e9dc
Nick Beaugeard <nick@hubone.com>
2017-05-30 10:58:47 +1000

    Revert "First Code Commit"
    This reverts commit 6c79e01a78765ac9fbf7b6c1ce95192572bafe06.

--- HubOne.XPM.PS/HubOne.PS/Scripts/GetGenericLists.ps1
+++
@@ -1,8 +0,0 @@
-﻿Import-Module "E:\OnePractice\OnePracticeApps\HubOne.PS\bin\Debug\HubOne.PS.dll"
-
-$siteURL = "https://hubone.sharepoint.com/sites/modernpractice/testdoc"
-$userName = "laurence@hubone.com"
-$passWord = "Hootsie656565$"
-
-Get-GenericSpList -SharePointUrl "https://hubone.sharepoint.com/sites/modernpractice/testdoc" `
--UserName "laurence@hubone.com" -Password "Hootsie656565$" -ListName "Staff" -OrderBy "StaffName" -IncludeRelationships $true
--- HubOne.XPM.PS/HubOne.PS/bin/Debug/Scripts/ConnectToOffice365.ps1
+++
@@ -1,7 +0,0 @@
-﻿Import-Module MSOnline 
-$O365Cred = Get-Credential 
-$O365Session = New-PSSession –ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $O365Cred -Authentication Basic -AllowRedirection 
-Import-PSSession $O365Session -AllowClobber 
-Connect-MsolService –Credential $O365Cred
-Import-Module 'C:\Program Files\SharePoint Online Management Shell\Microsoft.Online.SharePoint.PowerShell\Microsoft.Online.SharePoint.PowerShell.dll'
-Import-Module 'C:\Program Files\SharePoint Online Management Shell\Microsoft.Online.SharePoint.PowerShell\Microsoft.Online.SharePoint.Client.Tenant.dll'
